f9da16b3a2b36411ab92455393eaa11036dff9c2
이종찬 <53935322+qkskfka@users.noreply.github.com>
2024-07-24 22:59:23 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 5.18 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,11 +1,12 @@
 def solution(myString):
-    # "x"를 기준으로 문자열을 나누어 배열을 생성합니다.
+    # "x"를 기준으로 문자열을 분리합니다.
     parts = myString.split('x')
     
     # 빈 문자열을 제거합니다.
-    filtered_parts = [part for part in parts if part]
+    non_empty_parts = [part for part in parts if part]
     
-    # 배열을 사전순으로 정렬합니다.
-    sorted_parts = sorted(filtered_parts)
+    # 결과를 사전순으로 정렬합니다.
+    sorted_parts = sorted(non_empty_parts)
     
     return sorted_parts
+
