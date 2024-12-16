2ee3622480720050d0d4fddd4c95a8d4e1f10b57
이종찬 <53935322+qkskfka@users.noreply.github.com>
2024-12-16 16:50:53 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,14 +1,3 @@
 def solution(n, k):
-    # 양꼬치 가격 계산
-    yang_price = n * 12000
-    
-    # 서비스 음료수 개수 계산
-    free_drinks = n // 10
-    
-    # 음료수 가격 계산 (서비스 음료수를 제외한)
-    drink_price = (k - free_drinks) * 2000
-    
-    # 총 가격 계산
-    total_price = yang_price + drink_price
-    
-    return total_price
+    answer = 12000*n + 2000*k - 2000*(n//10)
+    return answer
