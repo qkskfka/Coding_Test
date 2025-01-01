b5a90327d7de787a7e4fd2da4ae728ea6fdedbef
이종찬 <53935322+qkskfka@users.noreply.github.com>
2025-01-01 23:42:07 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,12 +1,10 @@
 def solution(myString):
-    result = []
-    
-    for char in myString:
-        if char == 'a':
-            result.append('A')
-        elif char.isupper() and char != 'A':
-            result.append(char.lower())
+    res =""
+    for i in myString:
+        if i == "a":
+            res += "A"
+        elif i=="A":
+            res += "A"
         else:
-            result.append(char)
-    
-    return ''.join(result)
+            res += i.lower()
+    return res
