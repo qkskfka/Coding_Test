335f30a7c2e342d7246061087b5faeb225c4e39d
이종찬 <53935322+qkskfka@users.noreply.github.com>
2025-01-11 23:47:16 +0900

    [level 0] Title: x 사이의 개수, Time: 3.27 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,3 +1,8 @@
 def solution(myString):
-    tmp = myString.split("x")
-    return [len(i) for i in tmp]
+    # 문자열을 'x'를 기준으로 나눕니다.
+    parts = myString.split('x')
+    
+    # 각 부분 문자열의 길이를 리스트에 저장합니다.
+    lengths = [len(part) for part in parts]
+    
+    return lengths
