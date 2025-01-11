57964c460037cf8fb36adcf6052282b125af7524
이종찬 <53935322+qkskfka@users.noreply.github.com>
2025-01-12 00:01:41 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,3 +1,11 @@
 def solution(myString, pat):
-    a = "".join(["B" if char == "A" else "A" for char in myString])
-    return 1 if pat in a else 0
+    answer=''
+    for i in myString:
+        if i=='A':
+            answer+='B'
+        elif i=='B':
+            answer+='A'
+        else:
+            answer+=i
+
+    return int(pat in answer)
